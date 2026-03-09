09f33df9fdf645c9b6a1ce02175ec9b2869e8578
janaofficl <janarthananm1223@gmail.com>
2026-03-09 21:45:18 +0530

    Added backend changes in env files

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,20 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "task_manager"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306
 });
 
-db.connect(err => {
-    if (err) console.log(err);
-    else console.log("MySQL Connected ✅");
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
 });
 
 module.exports = db;
--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const boardRoutes = require("./routes/boardRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
+const PORT=process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/boards", boardRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("Server running on port 5000 🚀");
 });
