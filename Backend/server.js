b0215572af06bbc35101134b29578aab8729601b
janaofficl <janarthananm1223@gmail.com>
2026-03-10 12:00:20 +0530

    again cors

--- Backend/server.js
+++ Backend/server.js
@@ -9,11 +9,7 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 const PORT=process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin:"*"
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
