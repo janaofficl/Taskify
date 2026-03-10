0555b72c8a170615bdba6e9832f094405984a76b
janaofficl <janarthananm1223@gmail.com>
2026-03-10 11:14:04 +0530

    Backend ready for deployment

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port : process.env.DB_PORT || 3306
+    port : process.env.DB_PORT
 }); 
 
 db.connect((err) => {
